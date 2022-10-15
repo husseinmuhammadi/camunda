cffbddde7d84e3c7fcd21513e50c17ed7cb370bd
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-10-15 23:24:08 +0200

    Add integration test for BusinessModelsResource

--- web/src/main/java/com/camunda/bpmn/web/resources/BusinessModelsResource.java
+++ web/src/main/java/com/camunda/bpmn/web/resources/BusinessModelsResource.java
@@ -3,6 +3,7 @@ package com.camunda.bpmn.web.resources;
 import com.camunda.bpmn.generated.v1.api.BusinessModelApi;
 import com.camunda.bpmn.generated.v1.model.BusinessProcessModel;
 import com.camunda.bpmn.web.client.BusinessProcessModelingNotationProviderClient;
+import com.camunda.bpmn.web.exceptions.BpmnServiceException;
 import lombok.RequiredArgsConstructor;
 import org.camunda.bpm.model.bpmn.Bpmn;
 import org.camunda.bpm.model.bpmn.BpmnModelInstance;
@@ -23,6 +24,10 @@ public class BusinessModelsResource implements BusinessModelApi {
     @Override
     public ResponseEntity<String> getBusinessProcessingModelingNotation() {
         ResponseEntity<BusinessProcessModel> response = client.getBpmnDiagram();
+
+        if (response.getStatusCode().isError() || !response.hasBody())
+            throw new BpmnServiceException("Error getting response from remote service");
+
         String bpmn = response.getBody().getBpmn20Xml();
 
         BpmnModelInstance bpmnModelInstance = Bpmn.readModelFromStream(new ByteArrayInputStream(
--- web/src/test/java/com/camunda/bpmn/web/resources/BusinessModelsResourceIntegrationTest.java
+++ web/src/test/java/com/camunda/bpmn/web/resources/BusinessModelsResourceIntegrationTest.java
@@ -1,13 +1,29 @@
 package com.camunda.bpmn.web.resources;
 
+import com.camunda.bpmn.generated.v1.model.BusinessProcessModel;
 import com.camunda.bpmn.integration.IntegrationTest;
+import com.camunda.bpmn.web.client.BusinessProcessModelingNotationProviderClient;
+import org.camunda.bpm.model.bpmn.Bpmn;
+import org.camunda.bpm.model.bpmn.BpmnModelInstance;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+
+import static com.camunda.bpmn.utils.ResourceLoader.BusinessProcessModelNotation.INVALID_XML;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @IntegrationTest
 class BusinessModelsResourceIntegrationTest {
@@ -17,23 +33,46 @@ class BusinessModelsResourceIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    void shouldReturnInternalServerError_whenThereIsErrorInRemoteServerResponse() {
-//        mockMvc.perform(MockMvcRequestBuilders.get())
-    }
-
-    void shouldReturnXmlContent_whenGetBpmnDiagram(){
-
-    }
+    @SpyBean
+    private BusinessProcessModelingNotationProviderClient client;
 
     @Test
-    void shouldReturnValidBpmn_whenGetBpmnDiagram()throws Exception{
-        mockMvc.perform(get(ENDPOINT_BPMN_DIAGRAM_RESOURCE)
-                .content(MediaType.APPLICATION_JSON_VALUE))
+    void shouldReturnInternalServerError_whenThereIsErrorInRemoteServerResponse() throws Exception {
+        Mockito.when(client.getBpmnDiagram()).thenReturn(ResponseEntity.notFound().build());
+
+        mockMvc.perform(MockMvcRequestBuilders.get(ENDPOINT_BPMN_DIAGRAM_RESOURCE)).andExpectAll(
+                status().isInternalServerError()
+        );
+    }
+
+    @Test
+    void shouldReturnValidBpmn_whenGetBpmnDiagram() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get(ENDPOINT_BPMN_DIAGRAM_RESOURCE)
+                        .content(MediaType.APPLICATION_JSON_VALUE))
                 .andExpectAll(
                         status().isOk(),
-                        content().contentType(MediaType.APPLICATION_XML)
-                );
+                        content().contentTypeCompatibleWith(MediaType.APPLICATION_XML)
+                ).andReturn();
+
+        String bpmn = mvcResult.getResponse().getContentAsString();
+
+        BpmnModelInstance bpmnModelInstance = Bpmn.readModelFromStream(new ByteArrayInputStream(
+                bpmn.getBytes(StandardCharsets.UTF_8)));
+
+        assertDoesNotThrow(() -> Bpmn.validateModel(bpmnModelInstance));
+    }
+
+    @Test
+    void shouldReturnInternalServerError_whenRemoteServerResponseIsNotValidResponse() throws Exception {
+        final BusinessProcessModel bpm = new BusinessProcessModel()
+                .id("invoice:2:07bc6830-dc12-11e8-a8e9-0242ac110002")
+                .bpmn20Xml(INVALID_XML.readJson());
+
+        when(client.getBpmnDiagram()).thenReturn(ResponseEntity.ok(bpm));
+
+        mockMvc.perform(MockMvcRequestBuilders.get(ENDPOINT_BPMN_DIAGRAM_RESOURCE)).andExpectAll(
+                status().isInternalServerError()
+        );
     }
 
 }
